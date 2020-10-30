51d22dc79de9af9e0b59336eace63acf62ecae69
Jacek Ruszkowski <jac.ruszkowski@gmail.com>
2020-10-30 15:35:16 +0100

    kolejeny_update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -22,6 +23,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -32,7 +45,8 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
-    def form_valid(self, form):  # w models jest zrobiona funkcja, która przekierowuje na post-detail zaraz po stworzeniu
+    def form_valid(self,
+                   form):  # w models jest zrobiona funkcja, która przekierowuje na post-detail zaraz po stworzeniu
         form.instance.author = self.request.user
         return super().form_valid(form)
 
@@ -41,11 +55,12 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
-    def form_valid(self, form):  # w models jest zrobiona funkcja, która przekierowuje na post-detail zaraz po stworzeniu
+    def form_valid(self,
+                   form):  # w models jest zrobiona funkcja, która przekierowuje na post-detail zaraz po stworzeniu
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-    def test_func(self): # funkcja sprawdza czy ten kto chce edytowac post na pewno jest jego autorem
+    def test_func(self):  # funkcja sprawdza czy ten kto chce edytowac post na pewno jest jego autorem
         post = self.get_object()
         if self.request.user == post.author:
             return True
@@ -61,7 +76,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
-
 
 
 def about(request):
